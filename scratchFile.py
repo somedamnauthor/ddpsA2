e81d3b3a901264a5df11965a697844c25b16e875
somedamnauthor <sreeshnelp@gmail.com>
2022-12-16 05:46:44 +0100

    Added comments and remove unnecessary files

--- scratchFile.py
+++
@@ -1,8 +0,0 @@
-# mode = 'mapred'
-# if 'map' in mode:
-#     print "map in mode"
-# if 'reduce' in mode:
-#     print "reduce in mode"
-
-import multiprocessing
-print(multiprocessing.__version__)
--- wordcount_example.py
+++ wordcount_example.py
@@ -23,54 +23,98 @@ class WordCount(MapReduce):
         results: list containing key-value pairs where the key is word and value is count
         """
 
+        # Initialize list to store intermediate mapper output
         results = []
-        default_count = 1
-        # seperate line into words
-        for word in value.split():
-            if self.is_valid_word(word):
-                # lowercase words
-                results.append((word.lower(), default_count))
+
+        # At minimum, an encountered word appears once
+        count = 1
+
+        # split each line (value) into words (keys)
+        for single_word in value.split():
+
+            # Skip word if invalid
+            if self.is_word(single_word):
+
+                # attach a value (count) to each key
+                results.append((single_word.lower(), count))
+
         return results
 
-    def is_valid_word(self, word):
-        """Checks if the word is in the defined character range
-
-        :param word: word to check
+    def is_word(self, word):
+        
         """
+        Function to check if the given word is a valid word, i.e does not contain any special characters
+
+        Input - 
+        word: word given by the mapper() method
+        """
+
         return all(64 < ord(character) < 128 for character in word)
 
     def reducer(self, key, values):
-        """Reduce function implementation for the word count example
-        Note: Each line needs to be separated into words, and each word
-        needs to be converted to lower case.
+
         """
+        Function that implements the reducer and overrides the corresponding MapReduce class method 
+
+        Input - 
+        key, values
+
+        Outputs - 
+        key, wordcount - the key-value pair output that will later be consolidated
+        """
+
         wordcount = sum(value for value in values)
         return key, wordcount
 
 
 if __name__ == '__main__':
 
+    # Example argument list - 4 4 mapreduce final
+
+    # Hard-coding input and output directories
     input_dir = 'input'
     output_dir = 'output'
+
+    # Accept number of mappers and reducers for task from user
     n_mappers = int(sys.argv[1])
     n_reducers = int(sys.argv[2])
+
+    # Mode will be passed onto the run() method and decide whether the mapper controller, reducer controller or both will be invoked
+    # Can be 'map', 'reduce' or 'mapreduce' - the latter is used only in the case of non-distributed implementation
+    # This is given by the controller script
     mode = sys.argv[3]
+
+    # Boolean flag to signify whether the reduce operation (assuming mode is either 'reduce' or 'mapreduce') is the final one
+    # If the reduce operation is the final one the outputsd of the reducers are to be joined
+    # This is given by the controller script
     final_flag = sys.argv[4]
+
+    # The ID of the thread that is used to decide the chunk the reducer should work on
+    # Only used when mode is either 'reduce' or 'mapreduce'
+    # This is given by the controller script
     tid = sys.argv[5]
 
+    # Instantiate WordCount class with the user inputs
     word_count = WordCount(input_dir, output_dir, n_mappers, n_reducers)
+
+    # Run the map/reduce (or both) operations with the script inputs
     word_count.execute_mapreduce(mode=mode, tid=tid)
 
+    # Only consolidate results if the operation in question is the final reduce operation
     if final_flag == 'final':
-        result = (word for word in word_count.join_outputs(final_flag=final_flag))
-        print "-- Results of wordcount with parameters:", input_dir, output_dir, n_mappers, n_reducers
-        results_to_show = 50
-        print "-- Showing ", results_to_show," words:"
-        for i in range(results_to_show):
+
+        # Instantiate variable to read the joined outputs
+        reducer_result = (word for word in word_count.join_outputs(final_flag=final_flag))
+
+        print "\n\nPrinting top 25 most frequent words and their frequencies:"
+        for i in range(25):
+
             try:
-                word, count = result.next()
+                word, count = reducer_result.next()
                 print word, count
+
             except Exception:
                 pass
+
     else:
         print "Non-final Map/Reduce Completed"
